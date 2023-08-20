7f9a127a851d62a0cb4f467a92112b6cd6be4e77
OleksiiDmytruk <ljoshadmytruk@gmail.com>
2023-08-20 22:29:22 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import { Profile } from './Profile/Profile';
+import user from '../user.json';
 export const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Profile userData={user} />
+    </div>
+  );
 };
